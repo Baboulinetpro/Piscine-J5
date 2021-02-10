45d20cb19e364249701546c86cd45285507948f6
Antoine Nattier <antoinenattier@macbook-pro-de-antoine.home>
2021-02-10 18:51:19 +0100

    Task04

--- Task04/my_compute_power_rec.c
+++ Task04/my_compute_power_rec.c
@@ -1,10 +1,8 @@
 int my_compute_power_rec(int nb, int p)
 {
   if (p < 0)
-      return (0);
-  
+      return (0);  
   if (p == 0)
     return (1);
-   
   return (nb * my_compute_power_rec(nb, p - 1)); /* grace au -1 ex : 4*4*4 =64 alors que sans on aurait eu 4*4 = 16*16 = 256 */
 }
